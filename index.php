69aca3e05f61400be89a93182750fab68500285e
danqdenver <omnimizate@gmail.com>
2026-03-09 05:35:51 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,26 @@
-<?php
-echo "Proyecto funcionando";
-?>
+<!DOCTYPE html>
+<html>
+<head>
+<title>Generador de páginas</title>
+</head>
+<body>
+
+<h2>Generar página web</h2>
+
+<form action="generate.php" method="post">
+
+Nombre del archivo:<br>
+<input type="text" name="filename"><br><br>
+
+Título:<br>
+<input type="text" name="title"><br><br>
+
+Contenido:<br>
+<textarea name="content"></textarea><br><br>
+
+<button type="submit">Crear página</button>
+
+</form>
+
+</body>
+</html>
